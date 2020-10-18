dfbd6fe7e39e1e5b10a730c2dbb0933cb296e518
CALOGERO PISANO s273791 <s273791@studenti.polito.it>
2020-10-18 17:34:12 +0200

    update server.php

--- server/api/server.php
+++ server/api/server.php
@@ -5,7 +5,7 @@ require_once('dao.php');
 if(isset($_GET['url'])){
     $var = $_GET['url'];
     if($_SERVER['REQUEST_METHOD'] == 'GET'){
-        $number = intval(preg_replace('/[^0-9]+/', '', $var)); //id number user for query; it is found in  the url ex: localhost:80/users/id
+        $number = intval(preg_replace('/[^0-9]+/', '', $var)); //id number user for query; it is found in  the url ex: localhost:80/users/id if there is one
         switch($var){
             case "users":
                 //GET ALL USERS TABLE INFO
@@ -13,23 +13,53 @@ if(isset($_GET['url'])){
             break;
             case "tickets":
                 //GET ALL TICKETS INFO
-                echo "users to be implemented";
+                echo "tickets to be implemented";
             break;
             case "counters":
                 //GET ALL COUNTERS INFO
-                echo "users to be implemented";
+                echo "counters to be implemented";
             break;
             case "requests":
-                echo "users to be implemented";
+                echo "requests to be implemented";
                 //GET ALL REQUESTS INFO
             break;
             case "servedTickets":
-                echo "users to be implemented";
+                echo "servedtickets to be implemented";
                 //GET ALL SERVED TICKETS INFO
             break;
 
         }
     }
+    else if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $number = intval(preg_replace('/[^0-9]+/', '', $var)); //id number user for query; it is found in  the url ex: localhost:80/users/id if there is one
+        switch($var){
+            case "users":
+                //GET ALL USERS TABLE INFO
+                echo "users post to be implemented";
+            break;
+            case "tickets":
+                //GET ALL TICKETS INFO
+                echo "tickets post to be implemented";
+            break;
+            case "counters":
+                //GET ALL COUNTERS INFO
+                echo "counters post to be implemented";
+            break;
+            case "requests":
+                echo "requests post to be implemented";
+                //GET ALL REQUESTS INFO
+            break;
+            case "servedTickets":
+                echo "servedtickets post to be implemented";
+                //GET ALL SERVED TICKETS INFO
+            break;
+
+
+        }
+    }
+}
+else{
+    //nothing
 }
 
 
