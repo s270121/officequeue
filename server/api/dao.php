87298348a27f9521375b5bc91d195ec768dd0714
Rocco Petruzzi <s280126@studenti.polito.it>
2020-10-18 19:57:21 +0200

    feat: skeleton of some functions

--- server/api/dao.php
+++ server/api/dao.php
@@ -21,7 +21,39 @@ function checkLogin($username, $password){
     }
     else
         return 0;
+}
 
+//resets the queue every morning
+function resetTickets() {
+    global $db;
+
+    $sql = "DELETE FROM TICKETS";
+    $db->query($sql);
+}
+
+//insert the user in a queue according to the specified request type
+function getTicket($requestType){
+    $idTicket = getTicketId($requestType);
+    $estimatedTime = getEstimatedWaitingTime($requestType);
+
+    global $db;
+    $sql = "INSERT INTO TICKETS (idTicket, idRequest, estimatedTime) VALUES ($idTicket, $idRequest, $estimatedTime)";
+    $db->query($sql);
+}
+
+//TODO : to support getTicket()
+function getEstimatedWaitingTime ($requestType) {
+    return $time;
+}
+
+function getTicketId ($requestType) {
+    return $id;
+}
+
+//set isReady to true, given a counter ID
+function iAmReady($counterId) {
+    $sql = "UPDATE COUNTERS SET isReady=true WHERE idCounter='$counterId'";
+    $db->query($sql);
 }
 
 
