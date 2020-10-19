57825aa6db8406d2943fa20871ccd1e4d6967640
calogero <calo19296@gmail.com>
2020-10-19 10:18:42 +0200

    added put request switch in the server.php file

--- server/api/server.php
+++ server/api/server.php
@@ -34,24 +34,24 @@ if(isset($_GET['url'])){
         $number = intval(preg_replace('/[^0-9]+/', '', $var)); //id number user for query; it is found in  the url ex: localhost:80/users/id if there is one
         switch($var){
             case "users":
-                //GET ALL USERS TABLE INFO
+                
                 echo "users post to be implemented";
             break;
             case "tickets":
-                //GET ALL TICKETS INFO
+                
                 echo "tickets post to be implemented";
             break;
             case "counters":
-                //GET ALL COUNTERS INFO
+               
                 echo "counters post to be implemented";
             break;
             case "requests":
                 echo "requests post to be implemented";
-                //GET ALL REQUESTS INFO
+               
             break;
             case "servedTickets":
                 echo "servedtickets post to be implemented";
-                //GET ALL SERVED TICKETS INFO
+               
             break;
 
 
@@ -61,28 +61,58 @@ if(isset($_GET['url'])){
         $number = intval(preg_replace('/[^0-9]+/', '', $var)); //id number user for query; it is found in  the url ex: localhost:80/users/id if there is one
         switch($var){
             case "users":
-                //GET ALL USERS TABLE INFO
+                
                 echo "users delete to be implemented";
             break;
             case "tickets":
-                //GET ALL TICKETS INFO
+                
                 echo "tickets delete to be implemented";
             break;
             case "counters":
-                //GET ALL COUNTERS INFO
+                
                 echo "counters delete to be implemented";
             break;
             case "requests":
                 echo "requests delete to be implemented";
-                //GET ALL REQUESTS INFO
+                
             break;
             case "servedTickets":
                 echo "servedtickets delete to be implemented";
-                //GET ALL SERVED TICKETS INFO
+                
             break;
 
 
         }
+    }
+    else if($_SERVER['REQUEST_METHOD'] == 'PUT'){
+        $number = intval(preg_replace('/[^0-9]+/', '', $var)); //id number user for query; it is found in  the url ex: localhost:80/users/id if there is one
+        switch($var){
+            case "users":
+                
+                echo "users put to be implemented";
+            break;
+            case "tickets":
+                
+                echo "tickets put to be implemented";
+            break;
+            case "counters":
+                
+                echo "counters put to be implemented";
+            break;
+            case "requests":
+                echo "requests put to be implemented";
+                
+            break;
+            case "servedTickets":
+                echo "servedtickets put to be implemented";
+                
+            break;
+
+
+        }
+    }
+    else{
+        //nothing
     }
 }
 else{
