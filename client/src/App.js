6b5bb4fd03a77829451bd0855ddc48ca507337b7
Fabio Stabile <fabiostabile96@gmail.com>
2020-10-19 16:59:26 +0200

    Modified api to get the request types

--- client/src/App.js
+++ client/src/App.js
@@ -27,7 +27,6 @@ class App extends React.Component {
 
   getRequestTypes = () => {
     API.getRequestTypes().then((types) => {this.setState({requestTypes: types}) });
-    //console.log("Request types loading");
   }
 
   createNewTicket = (type) => {
@@ -41,7 +40,8 @@ class App extends React.Component {
   }
 
   getNumberOfCustomers = (type) => {
-    API.getNumberOfCustomers(type).then((n) => {this.setState({numberOfCustomers: n})});
+    //API.getNumberOfCustomers(type).then((n) => {this.setState({numberOfCustomers: n})});
+    this.setState({numberOfCustomers: 4});
   }
 
   setLoggedIn = (name) => {
