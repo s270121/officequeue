8818981abbe87ae9f9ccac43a236c6107ea92dbf
Massimiliano Torchio <>
2020-10-19 16:18:00 +0200

    Login form interface implemented, changed state "loggedin" into boolean true or false

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,19 @@ import React from 'react';
 import API from './API';
 import TopBar from './TopBar';
 import TicketForm from './TicketForm';
-import OfficerPage from './OfficerPage'
+import OfficerPage from './OfficerPage';
+import LoginForm from './LoginForm';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router, Route, Link, Switch, Redirect} from 'react-router-dom';
 import './App.css';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      loggedin: "false",            
+      loggedin: false,            
       requestTypes: [],
       ticketNumber: undefined,
       numberOfCustomers: undefined,
@@ -41,13 +44,26 @@ class App extends React.Component {
     API.getNumberOfCustomers(type).then((n) => {this.setState({numberOfCustomers: n})});
   }
 
+  setLoggedIn = (name) => {
+    this.setState({loggedin: true});
+  }
+
+  logout = () =>{
+    this.setState({loggedin: false});
+  }
+
   render(props) {
     return (
       <Router>
-        <TopBar loggedin = {this.state.loggedin}></TopBar>
+        <TopBar loggedin = {this.state.loggedin} logout={this.logout} ></TopBar>
         <Switch>
           <Route path="/login">
-            <p>Login form</p>
+            <Row className="height-100">
+              <Col sm={4}></Col>
+              <Col sm={4} className="below-nav">
+                <LoginForm setLoggedIn={this.setLoggedIn}/>
+              </Col>
+            </Row>
           </Route>
 
           <Route path="/officer">
